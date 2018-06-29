b8f2d733b6e3dc667faadd7512fe374a8d969823
Student <rederep@gmail.com>
2018-06-29 19:34:35 +0300

    Reflection, Override

---
+++ src/ua/ictloud/lessons/java8/reflection/MainR.java
@@ -0,0 +1,50 @@
+package ua.ictloud.lessons.java8.reflection;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+// Class - почти высший общий Class но не  Object
+
+public class MainR {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Class c = null;
+        try {
+            c = Class.forName("ua.ictloud.lessons.java8.reflection.RT");
+            System.out.println(c.getName());
+            System.out.println(c.getSimpleName());
+            for (Field field : c.getDeclaredFields()) {
+                System.out.println(field);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+// упрощенный вызов
+        Class c2 = RT.class;
+        System.out.println(c2.getName());
+        System.out.println(c2.getSimpleName());
+        for (Field field : c2.getDeclaredFields()) {
+            System.out.println(field);
+        }
+        System.out.println();
+        for (Method method : c2.getDeclaredMethods()) {
+            System.out.print("method: " + method.getName() + " --- have next annotations:\t");
+            for (Annotation annotation : method.getDeclaredAnnotations()) {
+                System.out.println(annotation);
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+//добираемся до переменной через рефлексию
+        RT r = new RT();
+        Field field = r.getClass().getDeclaredField("a");             //через рефлексию задали
+     //   Field field = RT.class.getDeclaredField("a");
+        field.setAccessible(true);
+        field.set(r, 66);
+        System.out.println(r.getA());               //через geter получили
+
+
+    }
+}
---
+++ src/ua/ictloud/lessons/java8/reflection/RT.java
@@ -0,0 +1,20 @@
+package ua.ictloud.lessons.java8.reflection;
+
+public class RT {
+    private int a;
+    public int b;
+    protected int c;
+
+    @Deprecated
+    public int get () {
+        return 0;
+    }
+
+    private void show(){
+        System.out.println("Ololol");
+    }
+
+    public int getA() {
+        return a;
+    }
+}
